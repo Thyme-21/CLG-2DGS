72c2e283d3fa88a2dd55255a0c5c521fb94228f8
Thyme-21 <1292966849@qq.com>
2026-03-15 17:10:51 +0800

    Add files via upload

---
+++ scripts/m360_eval.py
@@ -0,0 +1,80 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import os
+from argparse import ArgumentParser
+
+mipnerf360_outdoor_scenes = [ "garden"]
+# mipnerf360_outdoor_scenes = ["bicycle", "flowers", "garden", "stump", "treehill"]
+# mipnerf360_outdoor_scenes = ["bicycle","garden", "stump"]
+# mipnerf360_indoor_scenes = ["bonsai","room", "counter", "kitchen"]
+mipnerf360_indoor_scenes = [ "bonsai"]
+# tanks_and_temples_scenes = ["truck", "train"]
+# deep_blending_scenes = ["drjohnson", "playroom"]
+
+parser = ArgumentParser(description="Full evaluation script parameters")
+parser.add_argument("--skip_training", action="store_true")
+parser.add_argument("--skip_rendering", action="store_true")
+parser.add_argument("--skip_metrics", action="store_true")
+parser.add_argument("--output_path", default="output/m360")
+parser.add_argument("--n_views", type=int, default=12, help="number of training views")
+args, _ = parser.parse_known_args()
+
+all_scenes = []
+all_scenes.extend(mipnerf360_outdoor_scenes)
+all_scenes.extend(mipnerf360_indoor_scenes)
+# all_scenes.extend(tanks_and_temples_scenes)
+# all_scenes.extend(deep_blending_scenes)
+
+if not args.skip_training or not args.skip_rendering:
+    parser.add_argument('--mipnerf360', "-m360", required=True, type=str)
+    # parser.add_argument("--tanksandtemples", "-tat", required=True, type=str)
+    # parser.add_argument("--deepblending", "-db", required=True, type=str)
+    args = parser.parse_args()
+
+if not args.skip_training:
+    common_args = f" --quiet --eval --test_iterations -1 --n_views {args.n_views}"
+    for scene in mipnerf360_outdoor_scenes:
+        source = args.mipnerf360 + "/" + scene
+        os.system("python train.py -s " + source + " -i images_4 -m " + args.output_path + "/" + scene + common_args)
+    for scene in mipnerf360_indoor_scenes:
+        source = args.mipnerf360 + "/" + scene
+        os.system("python train.py -s " + source + " -i images_2 -m " + args.output_path + "/" + scene + common_args)
+    # for scene in tanks_and_temples_scenes:
+    # source = args.tanksandtemples + "/" + scene
+    # os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+    # for scene in deep_blending_scenes:
+    # source = args.deepblending + "/" + scene
+    # os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+
+if not args.skip_rendering:
+    all_sources = []
+    for scene in mipnerf360_outdoor_scenes:
+        all_sources.append(args.mipnerf360 + "/" + scene)
+    for scene in mipnerf360_indoor_scenes:
+        all_sources.append(args.mipnerf360 + "/" + scene)
+    # for scene in tanks_and_temples_scenes:
+    # all_sources.append(args.tanksandtemples + "/" + scene)
+    # for scene in deep_blending_scenes:
+    # all_sources.append(args.deepblending + "/" + scene)
+
+    common_args = f" --quiet --eval --skip_train --n_views {args.n_views}"
+
+    for scene, source in zip(all_scenes, all_sources):
+        os.system(
+            "python render.py --iteration 10000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+
+if not args.skip_metrics:
+    scenes_string = ""
+    for scene in all_scenes:
+        scenes_string += "\"" + args.output_path + "/" + scene + "\" "
+
+    os.system("python metrics.py -m " + scenes_string)
